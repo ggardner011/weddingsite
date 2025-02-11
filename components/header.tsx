2ce00dcaf229dbb592c10fcb3dbd0a39df8955ca
ggardner011 <73192025+ggardner011@users.noreply.github.com>
2025-02-11 10:13:19 -0800

    switch to next link

--- components/header.tsx
+++ components/header.tsx
@@ -1,31 +1,56 @@
-import React from 'react';
+import React from "react";
+import Link from "next/link";
 
 const Header: React.FC = () => {
-    return (
-        <header className="relative bg-cover bg-center h-64" style={{ backgroundImage: "url('/images/background.png')" }}>
-            <div className="absolute inset-0 bg-black opacity-50"></div>
-            <div className="relative z-10 flex flex-col items-center justify-center h-full  font-deco">
-                <h1 className="text-3xl md:text-4xl text-center font-extrabold  text-outline-green-header">Graeson and Elena&apos;s Wedding!</h1>
-                <nav className="mt-4">
-                    <ul className="flex space-x-4">
-                        <li>
-                            <a href="" className="text-yellow-dark font-bold  border-2 border-brown-dark bg-brown px-4 py-2 rounded">Home</a>
-                        </li>
-                        <li>
-                            <a href="/about" className="text-yellow-dark font-bold border-2 border-brown-dark bg-brown px-4 py-2 rounded">About</a>
-                        </li>
-                        <li>
-                            <a href="/qa" className="text-yellow-dark font-bold border-2 border-brown-dark bg-brown px-4 py-2 rounded">Q&A</a>
-                        </li>
-                    
-                        <li>
-                            <a href="/rsvp" className="text-yellow-dark font-bold border-2 border-brown-dark bg-brown px-4 py-2 rounded">RSVP</a>
-                        </li>
-                      </ul>  
-                </nav>
-            </div>
-        </header>
-    );
+  return (
+    <header
+      className="relative bg-cover bg-center h-64"
+      style={{ backgroundImage: "url('/images/background.png')" }}
+    >
+      <div className="absolute inset-0 bg-black opacity-50"></div>
+      <div className="relative z-10 flex flex-col items-center justify-center h-full  font-deco">
+        <h1 className="text-3xl md:text-4xl text-center font-extrabold  text-outline-green-header">
+          Graeson and Elena&apos;s Wedding!
+        </h1>
+        <nav className="mt-4">
+          <ul className="flex space-x-4">
+            <li>
+              <Link
+                href="/"
+                className="text-yellow-dark font-bold border-2 border-brown-dark bg-brown px-4 py-2 rounded"
+              >
+                Home
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/about"
+                className="text-yellow-dark font-bold border-2 border-brown-dark bg-brown px-4 py-2 rounded"
+              >
+                About
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/qa"
+                className="text-yellow-dark font-bold border-2 border-brown-dark bg-brown px-4 py-2 rounded"
+              >
+                Q&A
+              </Link>
+            </li>
+            <li>
+              <Link
+                href="/rsvp"
+                className="text-yellow-dark font-bold border-2 border-brown-dark bg-brown px-4 py-2 rounded"
+              >
+                RSVP
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
