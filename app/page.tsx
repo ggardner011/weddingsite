65278cef5e03bf99dd839f6c7438540cd60fa378
ggardner011 <73192025+ggardner011@users.noreply.github.com>
2025-02-10 21:59:51 -0800

    add apos fix

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@
 export default function Home() {
   return (
     <div className="bg-[#dfd7ac] min-h-screen">
-    <h1 className=" text-green-dark text-2xl md:text-4xl  font-bold p-8">Our Adventure!</h1>
-    <p className=" text-brown-dark text-md md:text-text-lg  p-4">This is your homepage with a full background color.</p>
+    <h1 className="text-green-dark text-2xl md:text-4xl  font-bold p-8">Our Adventure!</h1>
+    <p className="text-brown-dark text-md md:text-text-lg  p-4">This is your homepage with a full background color.</p>
   </div>
 );
 }
--- components/header.tsx
+++ components/header.tsx
@@ -5,7 +5,7 @@ const Header: React.FC = () => {
         <header className="relative bg-cover bg-center h-64" style={{ backgroundImage: "url('/images/background.png')" }}>
             <div className="absolute inset-0 bg-black opacity-50"></div>
             <div className="relative z-10 flex flex-col items-center justify-center h-full  font-deco">
-                <h1 className="text-3xl md:text-4xl text-center font-extrabold  text-outline-green-header">Graeson and Elena's Wedding!</h1>
+                <h1 className="text-3xl md:text-4xl text-center font-extrabold  text-outline-green-header">Graeson and Elena&apos;s Wedding!</h1>
                 <nav className="mt-4">
                     <ul className="flex space-x-4">
                         <li>
